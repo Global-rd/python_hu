5b3f764e2e1837e5ef2eecc58f16f43012292dc7
NagyIstvanGabor <63602487+NagyIstvanGabor@users.noreply.github.com>
2024-09-15 16:21:51 +0200

    updating homework_1 for testing rules

--- homeworks/peldabela/2_vars_datatypes/homework_1.py
+++ homeworks/peldabela/2_vars_datatypes/homework_1.py
@@ -1,2 +1,1 @@
-print("Did you think you'll find the answers here? At least you are navigating well in the repo :)")
-print("Test")
+print("Did you think you'll find the answers here? At least you are navigating well in the repo! :)")
