3ab2597c543bb41f9b27d741b79d765a343c871e
NagyIstvanGabor <63602487+NagyIstvanGabor@users.noreply.github.com>
2024-09-15 16:27:18 +0200

    Update homework_1.py

--- homeworks/peldabela/2_vars_datatypes/homework_1.py
+++ homeworks/peldabela/2_vars_datatypes/homework_1.py
@@ -1,1 +1,1 @@
-print("Did you think you'll find the answers here? At least you are navigating well in the repo! :)")
+print("Did you think you'll find the answers here? At least you are navigating well in the repo :)")
