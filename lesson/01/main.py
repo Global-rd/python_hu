db4f9068691a2a47716db94869e647d853bc1d86
NagyIstvanGabor <nagy.istvan.gabor@protonmail.com>
2024-09-22 16:28:33 +0200

    adding first lesson's .py files

---
+++ lesson/01/main.py
@@ -0,0 +1,30 @@
+print("Hello World!")
+
+# EGY SOROS KOMMENT / SINGLE LINE COMMENT
+
+"""
+THIS IS 
+A
+MULTI LINE COMMENT / TÖBB SOROS KOMMENT
+"""
+
+'''
+EZ IS EGY TÖBB SOROS KOMMENT
+'''
+
+## PRIMITÍV ADAT TÍPUSOK / PRIMITVE DATA TYPES
+print("Hello World again!") # STRING TYPE
+print('This is a string too') # STRING TYPE
+print("Hello 'World'")
+print('Hello "World"')
+
+print(1) # INTEGER / EGÉSZ SZÁM TÍPUS
+print(13132131) # INTEGER / EGÉSZ SZÁM TÍPUS
+
+print(3.14) # FLOAT / LEBEGŐPONTOS SZÁM
+
+print(True) #BOOLEAN / LOGIKAI ÉRTÉK
+print(False) #BOOLEAN / LOGIKAI ÉRTÉK
+
+print(None) # az információ hiánya
+
