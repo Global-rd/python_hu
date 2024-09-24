1105abc89a31f0ebc70e08d4a7726b144e6b4c16
balanagy23 <balanagy23@gmail.com>
2024-09-24 20:26:25 +0200

    1_Homework beadása
    Kommentek és Primitív adattípusok gyakorlása

---
+++ homeworks/nagybalazs/1_introduction/main.py
@@ -0,0 +1,19 @@
+"""
+THIS IS
+MY
+FIRST 
+PYTHON PROGRAM
+"""
+
+#PRIMITÍV ADAT TÍPUSOK
+
+print ("Hello World!")  #STRINGS
+
+print("123456789")      #INTEGERS
+
+print("3.141592")       #FLOAT
+
+print("True")           #BOOLEAN
+print("False")          #BOOLEAN
+
+print("None")           #NONE
