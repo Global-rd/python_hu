b142355fc3d654fca22f4190adfaf223c2773e48
barnaf03 <barnus03@gmail.com>
2024-09-24 21:24:58 +0200

    Javított

--- homeworks/feherbarnabas/1_introduction/testhw.py
+++ homeworks/feherbarnabas/1_introduction/testhw.py
@@ -2,6 +2,6 @@
 This is my first Python program.
 """
 print("String") #String
-print("4")      #Integer
-print("4,5")    #Float
-print("Hamis")  #Boolean
+print(4)        #Integer
+print(4,5)      #Float
+print(False)    #Boolean
