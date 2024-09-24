d86625e4936febcfa230cdb0ff60e695299f40de
snerbalu <nyoppart@gmail.com>
2024-09-24 18:37:05 +0200

    fixed homework schbalázs
    Rossz könyvtárba is tettem eredetileg, ha jól sejtem.

---
+++ homeworks/schreinerbalazs/1_introduction/main.py
@@ -0,0 +1,20 @@
+"""
+This is my first Python program.
+"""
+
+'''
+EZ IS EGY TÖBB SOROS KOMMENT MEGOLDÁS
+'''
+
+# PRIMITVE DATA TYPES
+
+print(1979) # INTEGER / EGÉSZ SZÁM TÍPUS
+
+print(2.23) # FLOAT / LEBEGŐPONTOS SZÁM
+
+print(True) 
+print(False) # BOTH BOOLEAN / LOGIKAI ÉRTÉK
+
+print(None) # NO INFORMATION / INFORMÁCIÓ HIÁNYA
+
+print("STRING") # STRING
