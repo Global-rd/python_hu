c385ec2d8817b71677ac477aa3873fb8dd10ef7a
slaca069 <slaca069@gmail.com>
2024-09-25 16:15:02 +0200

    Hibak javitasa - 1

--- homeworks/simonlaszlo/1_introduction/main.py
+++ homeworks/simonlaszlo/1_introduction/main.py
@@ -2,7 +2,6 @@
  This is my first Python program
 """
 print("szoveg") #string
-print("15") #integer
-print ("5.67") #float
-print("False") #Boolean
-print("") #None
+print(15) #integer
+print (5.67) #float
+print(False) #Boolean
