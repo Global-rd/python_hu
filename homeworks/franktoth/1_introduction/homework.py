337688540de281d36567075c6bda1f5a7af9a9b8
Frank <ferenc.zsolt.toth@gmail.com>
2024-09-25 17:48:32 +0200

    Homework 1 FT

---
+++ homeworks/franktoth/1_introduction/homework.py
@@ -0,0 +1,9 @@
+"""This is my first
+Python program"""
+
+#Python Data types
+print("python")     #string
+print(42)           #integer
+print (3.14)        #float
+print(True)         #Boolean
+print (None)        #None
