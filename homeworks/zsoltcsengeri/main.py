46219d94506d33ccfd31f0e97029b1e4f5b46686
zsoltcsengeri <zsoltcsengeri@yahoo.com>
2024-09-25 22:04:40 +0100

    Primitive data types added

---
+++ homeworks/zsoltcsengeri/main.py
@@ -0,0 +1,10 @@
+"""
+This is my first Python program
+Please find below the primitive data types
+
+"""
+print("Zsolt")  # this is a string str
+print(99)  # this is an integer int
+print(3.1415)  # this is a float
+print(True)  # this is a boolean bool
+print(None)  # represents the absence of a value
