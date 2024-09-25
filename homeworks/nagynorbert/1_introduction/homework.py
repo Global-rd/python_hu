c22f02498a9a530217c35e499d808df5a1158dc9
nagynorbi <nagynorbi@gmail.com>
2024-09-25 21:22:06 +0200

    Create homework.py

---
+++ homeworks/nagynorbert/1_introduction/homework.py
@@ -0,0 +1,22 @@
+# 1st homework - Nagy Norbert
+
+"""
+This is my First python program
+"""
+
+#int
+a = 5
+print("Integer type:", a)
+
+#float
+b = 3.14
+print("Float type:", b)
+
+#string
+my_str = "Example string"
+print("String type:", my_str)
+
+#bool
+my_bool = True
+print("Boolean type:", my_bool)
+
