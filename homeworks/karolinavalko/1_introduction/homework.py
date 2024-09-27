2e06f91fb970edf5dba87d991ddb8642e6154ec7
KarolinaValko <tabaluga86@gmail.com>
2024-09-27 13:54:42 +0200

    Here comes the code

---
+++ homeworks/karolinavalko/1_introduction/homework.py
@@ -0,0 +1,13 @@
+""" 
+This is 
+my first 
+Python program  
+"""
+
+# Examples for primitive data types
+
+print("My first homework")  # String type
+print(38)                   # Integer type
+print(5.19)                 # Float type
+print(True)                 # Boolean
+print(None)                 # Lack of information
