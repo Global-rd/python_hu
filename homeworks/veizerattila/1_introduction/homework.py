a7e33947f8b53e21989e3abf9fd8151b601c0a9d
vattti <attila.veizer@outlook.hu>
2024-09-28 19:03:37 +0200

    1. házi feladat

---
+++ homeworks/veizerattila/1_introduction/homework.py
@@ -0,0 +1,11 @@
+﻿"""This is my
+first Phyton
+program"""
+
+
+#Primitive Data Types / Primityv adattípusok
+print(9)        # int
+print(10.2)     # float
+print("apple")  # str
+print(None)     # None
+print(True)     # bool
