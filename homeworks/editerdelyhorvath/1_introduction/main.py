4ef76dbf8446d04bd074fbdf36e7c860e8912d5c
NyugtonMalac <nyugtonmalac@gmail.com>
2024-09-28 17:22:48 +0200

    1st homework

---
+++ homeworks/editerdelyhorvath/1_introduction/main.py
@@ -0,0 +1,9 @@
+'''
+This is my first Python program
+'''
+
+print(f"one example   -»  {type("one example")}") # str 
+print(f"2024          -»  {type(2024)}") # int
+print(f"3.14          -»  {type(3.14)}") # float
+print(f"True or False -»  {type(True)}") # bool
+print(f"None          -»  {type(None)}") # NoneType
