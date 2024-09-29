13c2534f0ca3fafd341744133b5b558d8e18d732
vali-toth-attila <vali.toth.attila@gmail.com>
2024-09-29 21:17:22 +0200

    1_vta_házi

---
+++ homeworks/valitothattila/1_introduction/main.py
@@ -0,0 +1,8 @@
+"""
+This is my first Python program
+"""
+print("hello world") #string type
+print(1313131313131) #integer
+print(3.14) # float
+print(True) # bolean
+print(None) # az információ hiánya
