f50cda8316d4b2946da989c9a7ca1aa96ac1cedf
Methmat <nemethmate1989@gmail.com>
2024-09-29 14:05:12 +0200

    nemethmate_homework

---
+++ homeworks/nemethmate/1_introduction/homework.py
@@ -0,0 +1,14 @@
+# String típus
+print("Hello, World!")  # str
+
+# Integer típus
+print(42)  # int
+
+# Float típus
+print(3.14)  # float
+
+# Boolean típus
+print(True)  # bool
+
+# NoneType típus
+print(None)  # NoneType
