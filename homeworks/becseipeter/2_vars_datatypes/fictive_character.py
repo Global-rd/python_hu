a55b4f19c04ce60f42aa4e92446c4073b29f805a
Becsei Peter <becsecse@gmail.com>
2024-10-01 21:43:17 +0200

    2.homework

---
+++ homeworks/becseipeter/2_vars_datatypes/fictive_character.py
@@ -0,0 +1,7 @@
+name = str(input("Whats your name?\n")).upper().strip()
+age = int(input("How old are you?\n"))
+age_in_days = age * 365 
+python_exp_in_years = int(input("How many years do you have in python?\n"))
+
+introduction = f"My carachter is {age} old. His name is {name} and he has {python_exp_in_years} experience."
+print(introduction)
