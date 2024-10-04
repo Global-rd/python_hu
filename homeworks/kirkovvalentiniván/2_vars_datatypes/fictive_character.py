4757545d72ca85dffa17f2c58432e3d76ba57e4f
vkirkov98 <vkirkov98@gmail.com>
2024-10-04 18:09:33 +0200

    kirkovvalentinivan/homework-2 Fictive_character & user_info finished
    After the previous branch I finished the two tasks with the bonus questions too.

--- homeworks/kirkovvalentiniván/2_vars_datatypes/fictive_character.py
+++ homeworks/kirkovvalentiniván/2_vars_datatypes/fictive_character.py
@@ -1,17 +1,18 @@
-character_name = input("What is your character's name?  ").title().strip()
+character_name = input("What is your character's name?  ").title().strip()  # Name input
 
-character_age = int(input("How old is your character?  "))
-character_age_days = round(character_age * 365, 0) 
+character_age = int(input("How old is your character?  "))                  #Age input
+character_age_days = round(character_age * 365, 0)                          #Age input in days
 
 python_exp = int(input("How many years of experience does he has in python programming? \
-Please enter this using only numbers:  "))
+Please enter this using only numbers:  "))                                  #Experience input
 
-python_learning_intention = input("Do you want to became an expert in python programming? (yes/no)  ").strip().lower()
-answ_learn_intent = True if python_learning_intention == "yes" else False
+python_learning_intention = input("Do you want to became an expert in python \
+programming? (yes/no)  ").strip().lower()                                   #Learning intention input
+answ_learn_intent = True if python_learning_intention == "yes" else False   #Boolean variable
 
-
-introduction = f"My character is {character_age} years old. His name is \
-{character_name} and he has {python_exp} years experience in python programming.\
+#Output
+introduction = f"My character is {character_age_days} days old. His name is \
+{character_name} and he has {python_exp} years experience in python programming. \
 He {"wants" if answ_learn_intent else "does not want"} to be a Python developer."
 
 print(introduction)
