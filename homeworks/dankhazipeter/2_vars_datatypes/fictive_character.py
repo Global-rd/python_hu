2cbc495fcaac06d9074b1680194adc2bd71539ec
Dankházi Péter <dankhazi.peter@webdream.hu>
2024-10-06 20:59:07 +0200

    Első házifeladat szorgalmival együtt

---
+++ homeworks/dankhazipeter/2_vars_datatypes/fictive_character.py
@@ -0,0 +1,22 @@
+# Adatok bekérése
+name = input("Add meg a karakter nevét: ")
+age_in_years = input("Add meg a karakter életkorát (évben): ")
+python_experience = input("Add meg a Python tapasztalatot években: ")
+wants_to_be_dev = input(
+    "Szeretné, hogy a karakter profi Python fejlesztő legyen? (yes/no): ")
+
+# Adattisztítás és típuskonverzió
+name = name.strip().title()
+age_in_years = int(age_in_years)
+python_experience = int(python_experience)
+
+# Ternary operator használata a válasz alapján
+wanna_be_pro_dev = True if wants_to_be_dev.lower() == "yes" else False
+
+# Életkor kiszámítása napokban
+# Feltételezzük, hogy ma van a születésnapja, nincs szökőév figyelembe véve
+age_in_days = age_in_years * 365
+
+# Kiírás
+print(f"My character is {age_in_days} days old. His/her name is {name} and he/she has {python_experience} years of experience. "
+      f"{'He/she wants to be a Python developer!' if wanna_be_pro_dev else 'He/she does not want to be a Python developer!'}")
