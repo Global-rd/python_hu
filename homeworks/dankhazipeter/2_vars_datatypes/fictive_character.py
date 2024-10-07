85bf7a0ff2b7623d5a21ec3fa484c2b07b07cbb9
Dankházi Péter <dankhazi.peter@webdream.hu>
2024-10-07 10:36:49 +0200

    Javítás a PR conversation alapján

--- homeworks/dankhazipeter/2_vars_datatypes/fictive_character.py
+++ homeworks/dankhazipeter/2_vars_datatypes/fictive_character.py
@@ -12,6 +12,8 @@ python_experience = int(python_experience)
 
 # Ternary operator használata a válasz alapján
 wanna_be_pro_dev = True if wants_to_be_dev.lower() == "yes" else False
+# A tényleges szöveget adjuk meg a változónak, a fenti részt azért hagyom bent, mert booleant kért a feladat :)
+wants_to_be_dev = "wants" if wanna_be_pro_dev else "does not want"
 
 # Életkor kiszámítása napokban
 # Feltételezzük, hogy ma van a születésnapja, nincs szökőév figyelembe véve
@@ -19,4 +21,4 @@ age_in_days = age_in_years * 365
 
 # Kiírás
 print(f"My character is {age_in_days} days old. His/her name is {name} and he/she has {python_experience} years of experience. "
-      f"{'He/she wants to be a Python developer!' if wanna_be_pro_dev else 'He/she does not want to be a Python developer!'}")
+      f"He/she {wants_to_be_dev} to be a Python developer!")
