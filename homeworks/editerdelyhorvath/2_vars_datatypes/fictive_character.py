957f346b09b709fda82fbe965ea8156b33f5eb9a
NyugtonMalac <nyugtonmalac@gmail.com>
2024-10-07 11:16:57 +0200

    Update fictive_character.py

--- homeworks/editerdelyhorvath/2_vars_datatypes/fictive_character.py
+++ homeworks/editerdelyhorvath/2_vars_datatypes/fictive_character.py
@@ -36,3 +36,18 @@ clear_terminal()
 # Print the result
 print(f"Your name is {name}, you are {age_in_days:.0f} days old and you has {python_exp_in_years} years of Python experience.")
 print(f"{'I am happy you want' if wants_to_be_dev else 'I am sad that you do not want'} to be a Python developer.\n\n")
+
+'''
+Megadott adatok:
+Enter your name: Nyugton Malac
+Enter your age (in years): 40
+Enter your Python experience (in years): 0.6
+Do you want to be a professional Python developer? (yes/no): yes
+
+Enter után tiszta terminalban:
+Your name is NYUGTON MALAC, you are 14600 days old and you has 0.6 years of Python experience.
+I am happy you want to be a Python developer.
+
+két sortöréssel az átláthatóság miatt.
+
+'''
