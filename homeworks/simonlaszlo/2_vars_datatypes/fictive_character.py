4588a4a9137b6de1c8ed6e27dd4372ed2b123229
slaca069 <slaca069@gmail.com>
2024-10-07 10:42:31 +0200

    SimonLaci/homevork_2_2

--- homeworks/simonlaszlo/2_vars_datatypes/fictive_character.py
+++ homeworks/simonlaszlo/2_vars_datatypes/fictive_character.py
@@ -1,9 +1,4 @@
-
-
-
-
-name_new = input("Name? ")
-name_new = name_new.strip().upper()
+name_new = input("Name? ").strip().upper()
 age_new =int(input("Age? "))
 age_day = int(age_new * 365.25)
 python_exp = int(input("Python experience in years? "))
@@ -11,5 +6,3 @@ professional_n = input("Professional Python experience? ")
 professional_n = professional_n.upper()
 want_prof="wants" if professional_n == "YES" else "does not want"
 print(f"My character is {age_day} days old. His/her name {name_new} and he/she has {python_exp} years experience. He/she {want_prof} to be Python developer.")
-
-
--- homeworks/simonlaszlo/2_vars_datatypes/user_info.py
+++ homeworks/simonlaszlo/2_vars_datatypes/user_info.py
@@ -1,4 +1,3 @@
-
 user_info={
     "name":"Mike",
     "age": 25,
@@ -15,7 +14,7 @@ user_info={
     }
 }
 skills_in = input("Kerek 4 programozasi nyelvet, vesszővel elválasztva, szóközök nélkül: ") # 1. adatbekeres
-user_info["skills"]=(skills_in.split(","))
+user_info["skills"]=skills_in.split(",")
 #print(user_info["skills"])
 
 user_info["favourite_meals"].sort() # 2. sorbarendezes
@@ -33,9 +32,7 @@ user_info["favourite_meals"].extend(user_info["favourite_meals"][2:4]) # 5. ket 
 user_info["favourite_meals"] = list(dict.fromkeys(user_info["favourite_meals"])) # 6. duplikacio torles
 #print(user_info["favourite_meals"])
 
-user_info["favourite_meals"].append(user_info["favourite_meals"][0]) # 7. elemek felcserelese
-user_info["favourite_meals"][0]=(user_info["favourite_meals"][-2])
-user_info["favourite_meals"].pop(3)
+user_info["favourite_meals"][0],user_info["favourite_meals"][-1]=user_info["favourite_meals"][-1],user_info["favourite_meals"][0]  # 7. elemek felcserelese
 #print(user_info["favourite_meals"])
 
 user_info["phone_contacts"]["Programmer"]="+36203263829" # 8. uj elem hozzaadas
@@ -50,6 +47,5 @@ user_info["phone_contacts"]["Architect"]=["+36203263929","+36703263929"]# 10. uj
 print(user_info["skills"]) # +1 az utolso 3 elem ellentetes sorrendben
 print(user_info["skills"][-3:][-1::-1])
 
-user_info["phone_contacts"]["Tim"]=user_info["phone_contacts"]["Tim2"] # +2 Tim atnevezese
-user_info["phone_contacts"].pop("Tim2",None)
+user_info["phone_contacts"]["Tim"]=user_info["phone_contacts"].pop("Tim2",None) # +2 Tim atnevezese
 #print(user_info["phone_contacts"])
