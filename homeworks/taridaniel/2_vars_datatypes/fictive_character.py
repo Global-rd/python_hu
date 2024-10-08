f728fa82146a1f24ac8167cae7db6f9f3a6c3a48
DanTar93 <taridani93@gmail.com>
2024-10-08 21:19:22 +0200

    2nd homework - updated version

--- homeworks/taridaniel/2_vars_datatypes/fictive_character.py
+++ homeworks/taridaniel/2_vars_datatypes/fictive_character.py
@@ -1,8 +1,11 @@
-name = "Daniel Tari"
-age = "30"
+name_old = input("Please, enter your name: ")
+name = name_old.strip().capitalize()
+
+age = input("Please, enter your age: ")
+
 python_exp_in_years = 2
 
-name = name.upper()
-age_in_days = int(age)*30*365
+age_in_days = int(age)*365
+print(age_in_days)
 
 print(f"My charater is {age_in_days} old. His name is {name} and he has {python_exp_in_years} years experience.")
--- homeworks/taridaniel/2_vars_datatypes/user_info.py
+++ homeworks/taridaniel/2_vars_datatypes/user_info.py
@@ -18,14 +18,18 @@ user_info = {
 #Exercise 1 - Asking for 4 programming langauges separated with comma without space. Converting the string to a list and adding it to the dictionary above
 user_info["skills"] = input("Add 4 programming languages: ").split()
 
+
 #Exercise 2 - Sorting the elements of favourite_meals list into alphabetical order
 user_info["favourite_meals"].sort()
+
 
 #Exercise 3 - Printing the second last element of favourite_meals list
 print(user_info["favourite_meals"][-2])
 
+
 #Exercise 4 - Adding spaghetti to favourite_meals list
 user_info["favourite_meals"].append("spaghetti")
+
 
 #Exercise 5 - Adding the 3rd and 4th element again to the current favourite_meals list
 user_info["favourite_meals"].extend(user_info["favourite_meals"][2:4])
@@ -34,15 +38,18 @@ user_info["favourite_meals"].extend(user_info["favourite_meals"][2:4])
 #Exercise 6 - Deleting the recently created duplicates
 user_info["favourite_meals"] = list(set(user_info["favourite_meals"]))
 
+
 #Exercise 7 - Swapping the first and last element of favourite_meals
-#Erre sajnos nem jöttem rá :(
+user_info["favourite_meals"][0], user_info["favourite_meals"][-1] = (user_info["favourite_meals"][-1], user_info["favourite_meals"][0],)
+
 
 #Exercise 8 - Adding a new element with name and phone number to phone_contacts
 user_info["phone_contacts"].update({"Robert": "+36209876543"})
 
+
 #Exercise 9 - Tim and Tim2 are the same people, delete Tim
 user_info["phone_contacts"].pop("Tim")
 
-#Exercise 10 - Add a new contact to phone_contacts list who has two different
-user_info["phone_contacts"].update({"Bob": "+36209876543" "," "+36208765432"})
 
+#Exercise 10 - Add a new contact to phone_contacts list who has two different
+user_info["phone_contacts"]["Bob"] = ["+36209876543", "36208765432"]
