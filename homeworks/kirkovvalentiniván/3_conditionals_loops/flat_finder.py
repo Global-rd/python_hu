daef2327bc9de6bede50cbb6ba6679a79bac2537
vkirkov98 <vkirkov98@gmail.com>
2024-10-09 23:43:53 +0200

    Created the folder and the files and finished the flat finder project

---
+++ homeworks/kirkovvalentiniván/3_conditionals_loops/flat_finder.py
@@ -0,0 +1,13 @@
+city = str(input("What is the name of the city? Write your answer here: ")).title().strip()
+rental_fee = int(input("How much is the rental fee? Write your answer here using only numbers: "))
+
+if city == ("New York" or "San Francisco") and (rental_fee < 4000):
+    print(f"You can rent an apartment in {city} for {rental_fee}$.")
+elif city == "Chicago":
+    print(f"You found an apartment in your ideal city, in {city} for {rental_fee}$")  
+elif city == "Washington":
+    print(f"You hate {city} and you don't want to live there.")  
+elif rental_fee < 3000:
+    print(f"You can rent an apartment in {city} for {rental_fee}$.")
+else:
+    print(f"The apartment does not suits your needs. Please search another one.")
