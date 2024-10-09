7305489ff71e5cb3e30c4353539bf9b9cf0d2d41
szabig <szabigesz@gmail.com>
2024-10-09 23:57:59 +0200

    3. Házifeladat elkészült

---
+++ homeworks/szabolcsgesztesi/3_conditionals_loops/flat_finder.py
@@ -0,0 +1,22 @@
+
+good_cities = ["New York", "San Fransisco"]
+worst_city = "Washington"
+best_city = "Chicago"
+
+city = input("Kérem adja meg a várost: ")
+price = int(input("Kérem adja meg a havi lakbér árát (USD): "))
+
+if city == best_city:
+    print(f"Sarah kedvenc városa {best_city}, bármi pénzert odaköltözne.")
+elif city == worst_city:
+    print(f"Sajnos Saharah számára {worst_city} elfogadhatatlan.")
+elif city in good_cities:
+    if price < 4000:
+        print(f"{city} elfogadható Sarah számára {price} dollárért.")
+    else:
+        print(f"{city} nem elfogadható Sarah számára {price} dollárért.")
+else:
+    if price < 3000:
+        print(f"{city} elfogadható Sarah számára {price} dollárért.")
+    else:
+        print(f"{city} nem elfogadható Sarah számára {price} dollárért.")
