09ac1a0cb20c7d51257c8807083ab031290e02c4
zsoltcsengeri <zsoltcsengeri@yahoo.com>
2024-10-09 16:49:35 +0100

    Refined flat_finder.py and 2nd draft of rock_paper_scissors.py

--- homeworks/zsoltcsengeri/3_conditional_loops/rock_paper_scissors.py
+++ homeworks/zsoltcsengeri/3_conditional_loops/rock_paper_scissors.py
@@ -1,25 +1,51 @@
-number_of_sets = int(input("Guys, how many sets do you want to play in a game? "))
+"""
+This is a rock, paper, scissors game.
+You can detremine at the begining of the game how many sets you want to play.
+"""
+
+# The wellknown 3 answers are stored in a tuple
+answers = ("rock", "paper", "scissors")
+
+# Ask for the number of sets the users want to play
+number_of_sets = int(
+    input("Guys, how many sets do you want to play in a game? "))
+
+while True:
+    if number_of_sets % 2 == 0:
+
+        print(
+            f"{number_of_sets} is even, therefore we can play draw. Please give me an odd number"
+        )
+        number_of_sets = int(
+            input("Guys, how many sets do you want to play in a game? "))
+
+    else:
+        print(f"The game will consist of {number_of_sets} sets")
+        break
+
+player_1 = input(
+    "What's your choice mate? rock, paper, scissors?").strip().lower()
+while True:
+    if player_1 in answers:
+        print(f"Player One:{player_1}")
+        break
+    else:
+        print(
+            f"{player_1} is not a valid choice, please make your choice from rock, paper, scissors words"
+        )
+        player_1 = input(
+            "What's your choice mate? rock, paper, scissors?").strip().lower()
 
 
-if number_of_sets % 2 == 0:
-    print(
-        f"{number_of_sets} is even, therefore we can play draw. Please give me an odd number"
-    )
-else:
-    print(f"The game will consist of {number_of_sets} sets")
-
-player_1 = input("What's your choice mate? rock, paper, scissors?")
-if player_1 == "rock" or player_1 == "paper" or player_1 == "scissors":
-    print(f"Player One:{player_1}")
-else:
-    print(
-        f"{player_1} is not a valid choice, please make your choice from rock, paper, scissors words"
-    )
-
-player_2 = input("What's your choice mate? rock, paper, scissors?")
-if player_2 == "rock" or player_2 == "paper" or player_2 == "scissors":
-    print(f"Player One:{player_2}")
-else:
-    print(
-        f"{player_2} is not a valid choice, please make your choice from rock, paper, scissors words"
-    )
+player_2 = input("What's your choice mate2? rock, paper, scissors?").strip().lower(
+)
+while True:
+    if player_2 in answers:
+        print(f"Player Two:{player_2}")
+        break
+    else:
+        print(
+            f"{player_2} is not a valid choice, please make your choice from rock, paper, scissors words"
+        )
+        player_2 = input(
+            "What's your choice mate2? rock, paper, scissors?").strip().lower()
