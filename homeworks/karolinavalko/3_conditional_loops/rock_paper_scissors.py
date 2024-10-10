f9764683b2dc2bf93f16d36edfca2a0cb5653ab6
KarolinaValko <tabaluga86@gmail.com>
2024-10-10 21:20:20 +0200

    Update rock_paper_scissors.py

--- homeworks/karolinavalko/3_conditional_loops/rock_paper_scissors.py
+++ homeworks/karolinavalko/3_conditional_loops/rock_paper_scissors.py
@@ -1,8 +1,25 @@
-rounds_to_play = input("Round number to play")
+from os import system
+
+system("cls")
 
 
-"""
-Rounds to be modulus 
+answer_selection = ["rock", "paper", "scissors"]
 
 
-"""
+while True:
+    rounds_to_play = int(input("Give an odd number for rounds: "))
+    if rounds_to_play % 2 != 0:
+        print(f"You will play {rounds_to_play} rounds.")
+        break
+
+round = 0
+
+while True:
+    player_1_answer = input("Choose rock, paper or scissor.")
+    if player_1_answer in answer_selection:
+        break
+
+while True:
+    player_2_answer = input("Choose rock, paper or scissor.")
+    if player_2_answer in answer_selection:
+        break
