c19ef755f9bfb982f444acb49c76fee1fe7947ea
gzlinsz <gzlinsz@yahoo.com>
2024-10-13 19:50:17 +0200

    zlinszkygyorgy/homework3

---
+++ homeworks/zlinszkygyorgy/3_conditionals_loops/flat_finder.py
@@ -0,0 +1,22 @@
+# input data (city and rental fee)
+city = input("In which city are you looking for a flat to rent? ").strip().title()
+rental_fee = int(input("What is the rental fee (USD)? "))
+
+# close out Washington
+if city == "Washington":
+    print(f"You cannot rent a flat in {city} for {rental_fee} USD")
+# Chicago for any price
+elif city == "Chicago":
+    print(f"You can rent a flat in {city} for {rental_fee} USD")
+# New York / San Francisco case
+elif city == "New York" or city == "San Francisco":
+    if rental_fee < 4000:
+        print(f"You can rent a flat in {city} for {rental_fee} USD")
+    else:
+        print(f"You cannot rent a flat in {city} for {rental_fee} USD")
+# all other cases            
+else:
+    if rental_fee <= 3000:
+        print(f"You can rent a flat in {city} for {rental_fee} USD")
+    else:
+        print(f"You cannot rent a flat in {city} for {rental_fee} USD")
