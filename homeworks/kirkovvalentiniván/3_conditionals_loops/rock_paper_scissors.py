fc8890a5d2d194087c37d7a0bdcca4425a5554f7
vkirkov98 <vkirkov98@gmail.com>
2024-10-14 09:07:01 +0200

    kirkovvalentinivan\homework-3\rock_paper_scissors
    Szia!
    Megcsináltam a második feladatot is, bár eléggé kifogott rajtam. A szorgalmival nem volt időm foglalkozni, de szeretném majd a későbbiekben (főleg, hogy most szünet jön) megcsinálni és gyakorolni. Ha azt majd később leadom, akkor azt milyen módon tehetem meg, ha a mostani 3. házi be lenne merge-elve?
    Köszi!

---
+++ homeworks/kirkovvalentiniván/3_conditionals_loops/rock_paper_scissors.py
@@ -0,0 +1,47 @@
+print("Welcome to the game. You will play rock-paper-scissors!")
+player_1_name = str(input("Player 1 please enter your name here: ")).title()
+player_2_name = str(input("Player 2 please enter your name here: ")).title()
+
+#Declaring the number of rounds
+while True:
+    rounds = int(input("How many rounds would you like to play? Please write your answer using only whole numbers:  "))
+    if rounds % 2 != 0:
+        break
+    print("Incorrect number. The outcome could be draw. To avoid this please write down an odd number.")
+
+#Necessary starting variables for the project
+correct_words = ["rock","paper","scissors"]
+player_1_score = 0
+player_2_score = 0
+
+#The game
+while rounds > 0:
+    while True:
+        player_1_word = str(input("Pick rock, paper or scissors and write your answer here: ")).lower()
+        if player_1_word in correct_words:
+            break
+        print("Incorrect word, please choose a correct one.")
+    
+    while True:         
+        player_2_word = str(input("Pick rock, paper or scissors and write your answer here: ")).lower()
+        if player_2_word in correct_words:
+            break
+        print("Incorrect word, please choose a correct one")
+
+    if player_1_word != player_2_word:
+        rounds -= 1
+        if  (player_1_word == "rock" and player_2_word == "scissors") or \
+            (player_1_word == "paper" and player_2_word == "rock") or \
+            (player_1_word == "scissors" and player_2_word == "paper"):
+            print(f"The winner of the round is {player_1_name}.")
+            player_1_score =+ 1
+        else:
+            print(f"The winner of the round is {player_2_name}.")
+            player_2_score += 1
+    else:
+        print("This round is even, please play again.")
+
+#Results
+if player_1_score > player_2_score:
+    print(f"The winner is {player_1_name}. {player_1_name} won by {player_1_score} points ahead of {player_2_name} who has {player_2_score} points.")
+print(f"The winner is {player_2_name}. {player_2_name} won by {player_2_score} points ahead of {player_1_name} who has {player_1_score} points.")
