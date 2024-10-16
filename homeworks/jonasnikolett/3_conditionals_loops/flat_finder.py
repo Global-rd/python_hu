46137680378ddff9aa6e10958e3c9e89bdd6d706
jonasnikolett90 <jonasnikolett90@gmail.com>
2024-10-16 21:05:05 +0200

    Update flat_finder.py

--- homeworks/jonasnikolett/3_conditionals_loops/flat_finder.py
+++ homeworks/jonasnikolett/3_conditionals_loops/flat_finder.py
@@ -12,40 +12,41 @@ Ezután a fentiek alapján printeld ki egy f-string használatával hogy az adot
 """
 
 # Város megadása
-rental_city = input(
-    "Sarah: In which city do you offer your flat for rented?")
+#rental_city = ("Washington", "Chicago")
+
+while True:
+    rental_city = input( "Sarah: In which city do you offer your flat for rented?")
 
 # ha Washington SOHA
-if rental_city == "Washington":
-    print(f"Agent: The city is {rental_city}.")
-    print(f"Sarah: Sorry, but I really don't like {rental_city}. No way!")
-    rental_city = input(
-        f"Sarah: Do you have any other apartment in other city?")
-
+    if rental_city == "Washington":
+        print(f"Agent: The city is {rental_city}.")
+        print(f"Sarah: Sorry, but I really don't like {rental_city}. No way!")
+        continue
 
 # ha Chicago nincs anyagi akadály
  
-if rental_city == "Chicago":
-    print(f"Agent: The city is {rental_city}.")
-    print(f"Sarah: My favorite place is {rental_city}, I'll take it!")
+    if rental_city == "Chicago":
+            print(f"Agent: The city is {rental_city}.")
+            print(f"Sarah: My favorite place is {rental_city}, I'll take it!")
+            continue
 
  
 # ha New York és San Fransisco, kevesebb mint 4000 USD
 # minden más 3000 USD
-else:
-    apartment_rent_price = int(input(f"Sarah: Thank you the city information, and how much is the rent for the apartment in dollars? "))
+     else:
+        apartment_rent_price = int(input(f"Sarah: Thank you the city information, and how much is the rent for the apartment in dollars? "))
+        print(f"Agent: So, this apartment in {rental_city} is {apartment_rent_price} dollars.")
 
-    print(f"Agent: So, this apartment in {rental_city} is {apartment_rent_price} dollars.")
-
-    if rental_city == "New York" or rental_city == "San Francisco":
-        if apartment_rent_price < 4000:
-            print(f"Sarah: Perfect! I love {rental_city}, and the price is good for me. I'll take it!")
+        if rental_city == "New York" or rental_city == "San Francisco":
+            if apartment_rent_price < 4000:
+                 print(f"Sarah: Perfect! I love {rental_city}, and the price is good for me. I'll take it!")
+            else:
+                print(f"Sarah: Sorry, I love {rental_city}, but the flat is expensive for me!")
+                print( rental_city = input(f"Sarah: Do you have any other apartment in other city?"))
         else:
-            print(f"Sarah: Sorry, I love {rental_city}, but the flat is expensive for me!")
-            print( rental_city = input(f"Sarah: Do you have any other apartment in other city?"))
-    else:
-        if apartment_rent_price <= 3000:
-            print(f"Sarah: Perfect! I love {rental_city}, and the price is good for me. I'll take it!")
-        else:
-            print(f"Sarah: Sorry, I love {rental_city}, but the flat is expensive for me!")
-            print( rental_city = input(f"Sarah: Do you have any other apartment in other city?"))
+            if apartment_rent_price <= 3000:
+                print(f"Sarah: Perfect! I love {rental_city}, and the price is good for me. I'll take it!")
+            else:
+                print(f"Sarah: Sorry, I love {rental_city}, but the flat is expensive for me!")
+                 print( rental_city = input(f"Sarah: Do you have any other apartment in other city?"))
+            break
