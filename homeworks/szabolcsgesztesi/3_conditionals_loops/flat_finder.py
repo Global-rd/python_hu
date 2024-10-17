6b3b51a3d278209bacd23e9b4b4ee372b21e2fd6
szabig <szabigesz@gmail.com>
2024-10-17 18:09:44 +0200

    3. Házifeladat - második javításai

--- homeworks/szabolcsgesztesi/3_conditionals_loops/flat_finder.py
+++ homeworks/szabolcsgesztesi/3_conditionals_loops/flat_finder.py
@@ -10,10 +10,9 @@ if city == best_city:
     print(f"Sarah kedvenc városa {best_city}, bármi pénzert odaköltözne.")
 elif city == worst_city:
     print(f"Sajnos Saharah számára {worst_city} elfogadhatatlan.")
-elif city in good_cities:
-    if price < 4000:
-        print(f"{city} elfogadható Sarah számára {price} dollárért.")
-    else:
-        print(f"{city} nem elfogadható Sarah számára {price} dollárért.")
+elif city in good_cities and price < 4000:
+    print(f"{city} elfogadható Sarah számára {price} dollárért.")
+elif price < 3000:
+    print(f"{city} elfogadható Sarah számára {price} dollárért.")
 else:
-    print(f"{city} {'elfogadható' if price < 3000 else 'nem elfogadható'} Sarah számára {price} dollárért.")
+    print(f"{city} nem elfogadható Sarah számára {price} dollárért.")
--- homeworks/szabolcsgesztesi/3_conditionals_loops/rock_paper_scissors.py
+++ homeworks/szabolcsgesztesi/3_conditionals_loops/rock_paper_scissors.py
@@ -15,21 +15,20 @@ while True:
 # A kiválasztott számnak megfelelő körök lejátszása:
 for rounds in range(chosen_rounds):
 
-    # Mindkét játékos választ
+    # Első játékos választ
     while True:
       player_1_turn = input("Első játékos választ: ")
       if player_1_turn in options:
            break
-
+    # Második játékos választ, de őt csak akkor engedi tovább ha mást választott mint az első játékos
     while True:
-      player_2_turn = input("Második játékos választ: ")
-      if player_2_turn in options:
-           break
-    
+        player_2_turn = input("Második játékos választ: ")
+        if player_2_turn in options and player_2_turn != player_1_turn:
+            break  # Kilépünk, ha érvényes és eltérő választás történt
+        print("A második játékos nem választhat ugyanazt! Próbáld újra.")
+
     # Játékszabály érvényesítése
-    if player_1_turn == player_2_turn:
-        print("Döntetlen!")
-    elif (player_1_turn == "rock" and player_2_turn == "scissors") or (player_1_turn == "scissors" and player_2_turn == "paper") or (player_1_turn == "paper" and player_2_turn == "rock"):
+    if (player_1_turn == "rock" and player_2_turn == "scissors") or (player_1_turn == "scissors" and player_2_turn == "paper") or (player_1_turn == "paper" and player_2_turn == "rock"):
         player1_score += 1
         print("Első játékos nyert ebben a körben!")
     else:
@@ -40,6 +39,4 @@ for rounds in range(chosen_rounds):
 print(f"Végeredmény: Első játékos {player1_score} pont, Második játékos {player2_score} pont")
 if player1_score > player2_score:
     print("Első játékos nyert!")
-elif player2_score > player1_score:
-    print("Második játékos nyert!")
-else: print("Döntetlen a játék!")
+else: print("Második játékos nyert!")
