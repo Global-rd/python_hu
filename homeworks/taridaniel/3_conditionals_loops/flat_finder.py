b70cf1699b253c3b104026f86f08924ae35786bf
DanTar93 <taridani93@gmail.com>
2024-10-17 14:24:12 +0200

    2nd homework - modified verson 2

--- homeworks/taridaniel/3_conditionals_loops/flat_finder.py
+++ homeworks/taridaniel/3_conditionals_loops/flat_finder.py
@@ -1,13 +1,13 @@
 city = input("Please enter an American city: ").title()
 price = int(input("Please enter the price (USD): "))
 
-if (city == "New York" or city == "San Francisco") and price < 4000:
+if city in ["New York", "San Francisco"] and price < 4000:
     print(f"I like these conditions in {city}, I want to live here")
 elif city == "Washington": 
     print(f"I hate {city}, I don't want to live in this city")
 elif city == "Chicago":
     print(f"I love {city} so much! The price of the rent doesn't matter, I want to live here!")
-elif (city != "New York" or city != "San Francisco" or city != "Washington" or city != "Chicago") and price <= 3000:
+elif price <= 3000:
     print(f"It's okay, this will work in {city} for me with these conditions")
 else:
     print("I don't like these conditions")
