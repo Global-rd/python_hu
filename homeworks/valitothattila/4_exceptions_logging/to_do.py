981de8d2ceeca439b9208d213c297b9dd7d24b7e
vali-toth-attila <vali.toth.attila@gmail.com>
2024-11-01 22:11:28 +0100

    valitothattila/homework-04v4
    Szervusz István,
    Korrigáltam a bekezdéseket, hogy egységes legyen.
    üdv.:
    Attila

--- homeworks/valitothattila/4_exceptions_logging/to_do.py
+++ homeworks/valitothattila/4_exceptions_logging/to_do.py
@@ -27,62 +27,61 @@ logger.setLevel(logging.DEBUG)
 
 # ------------------------ Menü = 1 -> task_list elem hozzáadása  -------------------------
 def add_to_tasklist(add_element):
-            main_task_list.append(f"{add_element}\n")
-            logger.info(f"Add elemnt to the task list: {add_element}.")
+    main_task_list.append(f"{add_element}\n")
+    logger.info(f"Add elemnt to the task list: {add_element}.")
 
 # ------------------------ Menü = 2 -> task_list elemeinek listázása -------------------------
-def list_tasklist():
-       
-            task_list = main_task_list
-            list_range = len(task_list)
-            if list_range > 0:
-                index_number = 1
-                for list_element in task_list:
-                    print(f"{index_number}: {list_element.strip()}")
-                    index_number += 1
-            else:
-                print("The list is empty!")
-            logger.info("List task elements.")      
+def list_tasklist(): 
+    task_list = main_task_list
+    list_range = len(task_list)
+    if list_range > 0:
+        index_number = 1
+        for list_element in task_list:
+            print(f"{index_number}: {list_element.strip()}")
+            index_number += 1
+    else:
+        print("The list is empty!")
+    logger.info("List task elements.")      
 
 
 # ------------------------ Menü = 3 -> task_list elem törlése -------------------------
 def del_element_of_list(del_index, del_list):
     if del_index > 0:                                           
-                del_range = len(del_list)                                         # lista hossz ellenőrzéséhez
-                while True:
-                    if del_range >= del_index:                                  # lista hossz ellenőrzése
-                        del del_list[del_index-1]                               # lista elem törlése index szerint 
-                        logger.info(f"The {del_index}. element deleted!")       # log bejegyzés a törlésről
-                        break
-                    else:
-                        print("The specified element was greater than the length of the list!")
-                        logger.info("The selected item number greater then the list of element number!")
-                    break
+        del_range = len(del_list)                                       # lista hossz ellenőrzéséhez
+        while True:
+            if del_range >= del_index:                                  # lista hossz ellenőrzése
+                del del_list[del_index-1]                               # lista elem törlése index szerint 
+                logger.info(f"The {del_index}. element deleted!")       # log bejegyzés a törlésről
+                break
+            else:
+                print("The specified element was greater than the length of the list!")
+                logger.info("The selected item number greater then the list of element number!")
+            break
     else:
         print(f"The specified element must be greater than zero!")
         logger.info("The specified element must be greater than zero!")
-    main_task_list = del_list                                                   # módosított listát visszaírjuk az eredeti listába
+    main_task_list = del_list                                            # módosított listát visszaírjuk az eredeti listába
     return main_task_list
 
 # ------------------------ Menü = 4 -> Fájlba írás és kilépés -------------------------
 def exit_function(write_task_list):
     try:
         with open(file_path, "w") as file:                      # file törlése mert piszok a maradt a file végén
-                file.write("")
+            file.write("")
         with open(file_path, "a") as file:                      # fájlba írás
             for new_task_list in write_task_list:
                 file.write(new_task_list)
         logger.info("Writing the new task list to a file is complete.")
     except Exception as e:
-            logger.error(f"{e}")       
+        logger.error(f"{e}")       
 
 
 # -------------------------- Főprogram kezdete ---------------------------
 
 # --------------------------- Fájl megnyitás -----------------------------
 try:
-    with open(file_path, "r") as file:                      # Fájl megnyitás
-                main_task_list = file.readlines()           # Fájl beolvasása memória listába
+    with open(file_path, "r") as file:                      # Fájl megnyitás                              
+        main_task_list = file.readlines()                   # Fájl beolvasása memória listába
 except Exception as e:
     logger.error(f"{e}")
     with open(file_path, "w") as file:                      # Fájl nyitási hiba miatt létrehozunk egy üres fájlt.
@@ -119,7 +118,7 @@ while True:
                 break
             else: print("Next step.")
         else: print("Invalid selection!")
-    except ValueError as error:
+    except ValueError as error:        
         logger.error("The specified element was not a number!")
     
 
