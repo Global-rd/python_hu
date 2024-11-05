c39a9316207249bea95dbaf1d279d61c4e60a4d3
BecseiPeter <becsecse@gmail.com>
2024-11-05 19:06:53 +0100

    becseipeter-homework_5

--- homeworks/becseipeter/5_oop/car_fleet_mgr.py
+++ homeworks/becseipeter/5_oop/car_fleet_mgr.py
@@ -42,35 +42,36 @@ class Fleet:
     def __str__(self):
         return f"Total performance of the fleet: {self.total_mileage()} km"
 
-    
 
-##############################################################################################
+if __name__ == "__main__":
 
-fleet = Fleet()
+    fleet.add_cars(car1)
+    fleet.add_cars(car2)
+    fleet.add_cars(car3)
+    fleet.add_cars(car4)
 
-fleet.add_cars(car1)
-fleet.add_cars(car2)
-fleet.add_cars(car3)
-fleet.add_cars(car4)
+    fleet = Fleet()
 
-car1 = Car("Audi", "Quattro", 1980)
-car2 = Car("Lamborghini", "Urus", 2024)
-car3 = Car("Seat", "Ibiza", 2018) 
-car4 = Car("Volkswagen", "Golf", 2015) 
+    car1 = Car("Audi", "Quattro", 1980)
+    car2 = Car("Lamborghini", "Urus", 2024)
+    car3 = Car("Seat", "Ibiza", 2018) 
+    car4 = Car("Volkswagen", "Golf", 2015) 
+
+    fleet = Fleet()
 
 
-car1.drive(km = 10)
-car2.drive(km = 250)
-car3.drive(km = 526)
-car4.drive(km = 1111)
+    car1.drive(km = 10)
+    car2.drive(km = 250)
+    car3.drive(km = 526)
+    car4.drive(km = 1111)
 
-car1.refuel(fuel = 20)
-car2.refuel(fuel = 56)
-car3.refuel(fuel = 50)
-car4.refuel(fuel = 65)
+    car1.refuel(fuel = 20)
+    car2.refuel(fuel = 56)
+    car3.refuel(fuel = 50)
+    car4.refuel(fuel = 65)
 
-print(fleet())
+    print(fleet())
 
-print(f"Traveled road: {fleet.total_mileage()} km\n")
+    print(f"Traveled road: {fleet.total_mileage()} km\n")
 
 
