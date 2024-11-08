59a58364d5e94355bbf1fe5fd502880e0e9d6639
GaalIstvanTamas <gaal.istvan.tamas@gmail.com>
2024-11-08 23:36:15 +0100

    Add changes

--- homeworks/gaalistvantamas/5_oop/car_fleet_mgr.py
+++ homeworks/gaalistvantamas/5_oop/car_fleet_mgr.py
@@ -15,27 +15,33 @@ class Car:
         self.fuel_level = fuel_level
 
     def drive(self, road_length_in_kilometres):
-        if road_length_in_kilometres < 1:
-            print("Please give a bigger number than 0!")
-        
-        elif not road_length_in_kilometres > int(self.fuel_level / self.FUEL_USAGE):
-            self.mileage += road_length_in_kilometres
-            self.fuel_level -= road_length_in_kilometres * self.FUEL_USAGE
-            print(f"You drove {road_length_in_kilometres} km and the fuel usage of the trip was {round(100 - self.fuel_level)}%!")
+        if self.fuel_level == 0.0:
+            print("You can't drive, because the tank is empty!")
 
         else:
-            print("There is no enough fuel in the car!")
-            print(f"You can drive maximum: {self.fuel_level / self.FUEL_USAGE} km!")
+            if road_length_in_kilometres <= 0:
+                print("Please give a bigger number than 0!")
+
+            elif not road_length_in_kilometres > int(self.fuel_level / self.FUEL_USAGE):
+                self.mileage += road_length_in_kilometres
+                self.fuel_level -= road_length_in_kilometres * self.FUEL_USAGE
+                print(f"You drove {road_length_in_kilometres} km and the fuel usage of the trip was {(road_length_in_kilometres * self.FUEL_USAGE):.2f}%!")
+
+            else:
+                print(f"You drive {(self.fuel_level / self.FUEL_USAGE)} km and the tank is empty!")
+                self.mileage += (self.fuel_level / self.FUEL_USAGE)
+                self.fuel_level = 0.0
 
     def refuel(self, refuel_percents):
-        if refuel_percents < 1:
+        if refuel_percents <= 0:
             print("Please give a bigger number than 0!")
         
         elif refuel_percents > (100.0 - self.fuel_level):
-            print(f"You can refuel maximum {round(100 - self.fuel_level)}%!")
+            print(f"You can refuel maximum {(100 - self.fuel_level):.2f}%!")
         
         else:
             self.fuel_level += refuel_percents
+            print(f"You refuel {refuel_percents:.2f}%!")
 
     def display_info(self):
         print(f"Brand: {self.brand}")
@@ -54,10 +60,13 @@ class Fleet:
         print(f"{car_to_be_added.brand} {car_to_be_added.model} {car_to_be_added.year}\n")
 
     def remove_car(self, car_to_be_removed):
-        self.cars.remove(car_to_be_removed)
-        print(f"This car has to be removed the fleet:") 
-        print(f"{car_to_be_removed.brand} {car_to_be_removed.model} {car_to_be_removed.year}\n")
-    
+        if car_to_be_removed in self.cars:
+            self.cars.remove(car_to_be_removed)
+            print(f"This car has to be removed the fleet:") 
+            print(f"{car_to_be_removed.brand} {car_to_be_removed.model} {car_to_be_removed.year}\n")
+        else:
+            print("This car is not in fleet!\n")
+
     def all_kilometers(self):
         all_km = 0
         for car in self.cars:
@@ -74,9 +83,10 @@ class Fleet:
 
 car_fleet = Fleet()
 
-car_1 =Car("Ford", "Focus", 2002)
-car_2 =Car("Ford", "Mondeo", 2021)
-car_3 =Car("Lada", "Samara", 1994)
+car_1 = Car("Ford", "Focus", 2002)
+car_2 = Car("Ford", "Mondeo", 2021)
+car_3 = Car("Lada", "Samara", 1994)
+car_4 = Car("Opel","Astra f", 1998)
 
 
 car_fleet.add_car(car_1)
@@ -84,13 +94,17 @@ car_fleet.add_car(car_2)
 car_fleet.add_car(car_3)
 
 car_1.drive(50)
+car_1.drive(-50)
+car_1.display_info()
+car_1.drive(0.5)
 car_1.display_info()
 car_1.drive(5000)
-car_1.display_info()
-car_1.drive(-10)
+car_1.drive(0.5)
 car_1.display_info()
 
 car_1.refuel(10)
+car_1.display_info()
+car_1.refuel(0.5)
 car_1.display_info()
 car_1.refuel(-10)
 car_1.display_info()
@@ -99,8 +113,8 @@ car_2.drive(200)
 car_2.display_info()
 
 car_fleet.remove_car(car_3)
+car_fleet.remove_car(car_4)
 
 car_fleet.all_kilometers()
 car_fleet.car_info()
 car_fleet.fleet_info()
-
