263ca1ca0865533a16feda8c084e8ee4e2ee1b77
Methmat <nemethmate1989@gmail.com>
2024-11-10 17:13:21 +0100

    nemethmate_homework_update

--- homeworks/nemethmate/6_testing/bankaccount.py
+++ homeworks/nemethmate/6_testing/bankaccount.py
@@ -1,17 +1,19 @@
 class BankAccount:
     def __init__(self, owner: str, balance: float = 0.0):
         # Konstruktor: beállítja a tulajdonost és az egyenleget, ellenőrzi, hogy az egyenleg nem negatív.
+        if isinstance(balance, str):
+            raise TypeError("Balance must be a number.")
         if balance < 0:
             raise ValueError("Initial balance cannot be negative.")
         self.owner = owner
         self.balance = balance
-    
+
     def deposit(self, amount: float):
         # Befizetés: ellenőrzi, hogy az összeg pozitív, majd hozzáadja az egyenleghez.
         if amount <= 0:
             raise ValueError("Deposit amount must be positive.")
         self.balance += amount
-    
+
     def withdraw(self, amount: float):
         # Kivétel: ellenőrzi, hogy az összeg pozitív és elérhető az egyenlegből, majd levonja az összeget.
         if amount <= 0:
@@ -26,7 +28,7 @@ class BankAccount:
             raise TypeError("Target must be a BankAccount instance.")
         self.withdraw(amount)
         target_account.deposit(amount)
-    
+
     def get_balance(self):
         # Egyenleg lekérdezése: visszaadja az aktuális egyenleget.
         return self.balance
