4135ebedc70b4cc7dc920517a43503b95c408451
vkirkov98 <vkirkov98@gmail.com>
2024-11-11 14:54:14 +0100

    kirkovvalentinivan\homework-6
    Szia István!
    
    Pusholom a discordon megbeszéltek szerit megkévő hibával egybevéve. Bizonyára lehetnek más észrevételeid is, de arra kifejezetten kíváncsi vagyok, mi lehet a gond a test_transfer function-nél.
    
    Köszi!

---
+++ homeworks/kirkovvalentiniván/06_testing/test_bankaccount.py
@@ -0,0 +1,51 @@
+import pytest
+from bankaccount import BankAccount
+from unittest.mock import patch
+
+@pytest.fixture
+def bankaccount_1():
+    return BankAccount("David", 200)
+
+@pytest.fixture
+def bankaccount_2():
+    return BankAccount("Emma", 550)
+
+@pytest.fixture
+def invalid_bankaccount():
+    return BankAccount("Invalid Account", -100)
+
+@pytest.mark.parametrize("deposit_amount, expected_balance, expected_exception", [
+    (30, 230, None), #normal case
+    (0, 200, ValueError), #Error: adding zero to the actual balance 
+    (-30, 200, ValueError) #Error: adding negative number to the actual balance
+])
+
+def test_deposit(bankaccount_1, deposit_amount, expected_balance, expected_exception):
+    if expected_exception:
+        with pytest.raises(expected_exception):
+            bankaccount_1.deposit(deposit_amount)
+    else:
+        bankaccount_1.deposit(deposit_amount)
+        assert bankaccount_1.get_balance() == expected_balance
+
+def test_withdraw(bankaccount_1):
+    bankaccount_1.withdraw(50)
+    assert bankaccount_1.get_balance() == 150
+
+    with pytest.raises(ValueError):
+        bankaccount_1.withdraw(2000)
+
+    with pytest.raises(ValueError):
+        bankaccount_1.withdraw(0)
+
+def test_transfer(bankaccount_1, bankaccount_2):
+    bankaccount_1.transfer(50, bankaccount_2)
+    
+    assert bankaccount_1.get_balance() ==200
+    assert bankaccount_2.get_balance() ==600
+
+def test_transfer_to_self(bankaccount_1):
+    initial_balance = bankaccount_1.get_balance()
+
+    bankaccount_1.transfer(50, bankaccount_1)
+    assert bankaccount_1.get_balance() == initial_balance
