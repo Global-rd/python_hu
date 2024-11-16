b558542bba295a9cfa24b6f5559816dfa1de5ee1
zsoltcsengeri <zsoltcsengeri@yahoo.com>
2024-11-16 16:15:36 +0000

    Create DataFrame and CSV

--- homeworks/zsoltcsengeri/7_extract_transform/coin_list_api.py
+++ homeworks/zsoltcsengeri/7_extract_transform/coin_list_api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 
 url = "https://api.coingecko.com/api/v3/coins/markets"
 
@@ -14,4 +15,11 @@ response = requests.get(url=url, params=params, headers=headers)
 
 # Parse the response JSON and pretty-print it
 data = response.json()  # Convert the response to a Python dictionary
-print(json.dumps(data, indent=4))  # Pretty-print with indentation
+#print(json.dumps(data, indent=4))  # Pretty-print with indentation
+
+# Convert the Python dictionary data to DataFrame
+df = pd.DataFrame(data)
+print(df)
+
+#save a DataFrame to a CSV file
+df.to_csv('./coin_list_250.csv', index=False) #index=False prevents an index column from being added
