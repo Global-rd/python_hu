af080914ccf763f8242ac632e1a9d488739aed69
vkirkov98 <vkirkov98@gmail.com>
2024-11-18 17:27:34 +0100

    kirkovvalentinivan\homework-7\updated
    Szia István!
    
    Köszi a tippeket, elméletileg most úgy van módosítva, ahogy te is elképzelted.

--- homeworks/kirkovvalentiniván/7_extract_transform/cripto_investment.py
+++ homeworks/kirkovvalentiniván/7_extract_transform/cripto_investment.py
@@ -3,14 +3,9 @@ import pandas as pd
 
 class CoingeckoAPI: 
     
-    def __init__(self,vs_currency = "usd", order = "market_cap_desc", per_page = 250, page = 1):
-        self.base_url = "https://api.coingecko.com/api/v3/coins/markets"
-        self.params = {
-        "vs_currency": vs_currency,
-        "order": order,
-        "per_page": per_page,
-        "page": page
-        }
+    def __init__(self, base_url, params):
+        self.base_url = base_url
+        self.params = params 
     
         self.raw_data = None
         self.df = None
@@ -23,12 +18,14 @@ class CoingeckoAPI:
         else:
             raise Exception(f" API error: {response.status_code}, {response.text}")
 
-    def create_dataframe(self):
-        """Convert the get_data into a Pandas DataFrame."""
-        if self.raw_data is None:
-            raise ValueError("There is no data after the pull. First run the get_data method!")
-        self.df = pd.DataFrame(self.raw_data)
-        print("DataFrame is created successfully.")
+class ResponseTransformation:    
+
+    def __init__(self, raw_data):
+        
+        if not raw_data:
+            raise ValueError("There is no raw data or something went wrong.")
+        self.df = pd.DataFrame(raw_data)
+        print("Dataframe is created successfully.")
         
     def count_missing_values(self):
         """Count missing values in the DataFrame."""
@@ -73,35 +70,43 @@ class CoingeckoAPI:
 # Run the process
 if __name__ == "__main__":
 
+    base_url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        "vs_currency": "usd",
+        "order": "market_cap_desc",
+        "per_page": 250,
+        "page": 1
+    }
+    
     #Set up connection to the API
-    api = CoingeckoAPI(vs_currency="usd", order="market_cap_desc", per_page=250, page=1)
+    api = CoingeckoAPI(base_url, params)
 
     #Extract the data
     api.get_data()
 
     #Create the dataframe using the function
-    api.create_dataframe()
+    transformer = ResponseTransformation(api.raw_data)
 
     #Counting empty cells
-    missing_values = api.count_missing_values()
+    missing_values = transformer.count_missing_values()
     print("Empty values per columns: ")
     print(missing_values)
 
     #Calculating total market cap
-    total_market_cap = api.calculate_total_market_cap()
+    total_market_cap = transformer.calculate_total_market_cap()
     print(f"Total market cap is: {total_market_cap}.")
 
     #Top 50 crypto currency group by the "current_price" column
-    top50df = api.get_top_n("current_price", 50)
+    top50df = transformer.get_top_n("current_price", 50)
     print(f"Top 50 crypto currencies based on the 'current_price' are: ")
     print(top50df)
 
     #Sort based on the price change
-    sorted_top50_df = api.sort_by_column("price_change_percentage_24h", ascending=False)
+    sorted_top50_df = transformer.sort_by_column("price_change_percentage_24h", ascending=False)
     print("Top 50 crypto currency based on the price change percentage: ")
     print(sorted_top50_df)
 
     # 7. Add new column for representing the change of the diredtion of the prices
-    api.add_change_direction_column("price_change_percentage_24h", "change_direction")
+    transformer.add_change_direction_column("price_change_percentage_24h", "change_direction")
     print("The DataFrame updated with the direction changed column: ")
-    print(api.df.head())
+    print(transformer.df.head())
