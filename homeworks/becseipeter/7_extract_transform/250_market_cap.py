f1930e335ce182c8d7af8d522fedd69929f0aed2
BecseiPeter <becsecse@gmail.com>
2024-11-21 18:09:09 +0100

    becseipeter/homework_7

--- homeworks/becseipeter/7_extract_transform/250_market_cap.py
+++ homeworks/becseipeter/7_extract_transform/250_market_cap.py
@@ -3,9 +3,14 @@ import pandas as pd
 
 url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
 
-headers = {"accept": "application/json"}
+params = {
+    "vs_currency": "usd",  
+    "order": "market_cap_desc", 
+    "per_page": 250,  
+    "page": 1  
+}
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, params=params)
 
 data = response.json()  
 limited_data = data[:250]  # Get only the first 250 items 
