3d789866e21d90456ca3ee1ac3bcfa451fc1e7ed
NagyIstvanGabor <nagy.istvan.gabor@protonmail.com>
2024-11-25 19:33:04 +0100

    lesson 17 done

---
+++ lessons/17/movies_api.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from uuid import UUID
+from models import Movie, MovieRequest, MovieResponse
+from database import Base, engine, get_db
+from typing import List
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+@app.get("/movies/", response_model=List[MovieResponse])
+async def get_movies(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Movie)) #SELECT * FROM movies
+    movies = result.scalars().all()
+    return movies
+
+@app.post("/movies/", response_model=MovieResponse)
+async def add_movie(movie: MovieRequest, db: AsyncSession = Depends(get_db)):
+    new_movie = Movie(**movie.dict())
+    db.add(new_movie)
+    await db.commit()
+    await db.refresh(new_movie)
+    return new_movie
+
+@app.get("/movies/{movie_id}", response_model=MovieResponse)
+async def get_movie(movie_id: UUID, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Movie).where(Movie.id == str(movie_id)))
+    movie = result.scalar_one_or_none() 
+    if not movie: #guard clause
+        raise HTTPException(status_code=404, detail=f"Movie id {movie_id} not found")
+    return movie
+
+@app.put("/movies/{movie_id}", response_model=MovieResponse)
+async def update_movie(movie_id: UUID, movie_update: MovieRequest, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Movie).where(Movie.id == str(movie_id)))
+    movie = result.scalar_one_or_none()
+    if not movie: #guard clause
+        raise HTTPException(status_code=404, detail=f"Movie id {movie_id} not found")
+
+    for key, value in movie_update.dict(exclude_unset=True).items():
+        setattr(movie, key, value) # title update: title.value = "New Title" -> movie.title = "New Title"
+
+    db.add(movie)
+    await db.commit()
+    await db.refresh(movie)
+    return movie
+
+@app.delete("/movies/{movie_id}", response_model=MovieResponse)
+async def delete_movie(movie_id: UUID, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Movie).where(Movie.id == str(movie_id)))
+    movie = result.scalar_one_or_none()
+    if not movie: #guard clause
+        raise HTTPException(status_code=404, detail=f"Movie id {movie_id} not found")
+    
+    await db.delete(movie)
+    await db.commit()
+    return movie
