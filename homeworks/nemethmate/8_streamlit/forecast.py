9fbe6a5418f9ce95f97d29a685a25b36893bba23
Methmat <nemethmate1989@gmail.com>
2024-11-27 09:54:11 +0100

    nemethmate_homework_8

---
+++ homeworks/nemethmate/8_streamlit/forecast.py
@@ -0,0 +1,9 @@
+import requests
+
+def get_forecast(city, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
