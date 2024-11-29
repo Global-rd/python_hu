a1a1a25a00d24fffb4ca592aa1eaea29682d05e2
GaalIstvanTamas <gaal.istvan.tamas@gmail.com>
2024-11-29 19:38:33 +0100

    Add change to single responsibility

--- homeworks/gaalistvantamas/8_streamlit/weather_app.py
+++ homeworks/gaalistvantamas/8_streamlit/weather_app.py
@@ -11,62 +11,54 @@ API_KEY = st.secrets["openweathermap"]["api_key"]
 WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
 FORECAST_URL= "http://api.openweathermap.org/data/2.5/forecast"
 
-#weather and forecast
+#fetch data
 @st.cache_data(ttl=3600)
-def fetchweather_and_forecast_data(city):
-    weather_and_forecast_data = {}
+def fetchweather_and_forecast_data(city,url):
     params = {'appid': API_KEY,
                 "q" : city,
                 "units" : "metric"}
     
-    response_weather = requests.get(f"{WEATHER_URL}", params=params)
+    response = requests.get(f"{url}", params=params)
    
-    if response_weather.status_code == 200:
-        weather_and_forecast_data["weather"] = response_weather.json()
+    if response.status_code == 200:
+        return response.json()
     else:
-        st.error(f" Failed to fetch data: {response_weather.status_code} - {response_weather.text}")
+        st.error(f" Failed to fetch data: {response.status_code} - {response.text}")
         return None
-    
-    response_forecast = requests.get(f"{FORECAST_URL}", params=params)
-   
-    if response_forecast.status_code == 200:
-        weather_and_forecast_data["forecast"] = response_forecast.json()
-    else:
-        st.error(f" Failed to fetch data: {response_forecast.status_code} - {response_forecast.text}")
-        return None
-
-    return weather_and_forecast_data
 
 st.title("Robot Dreams Python - Weather Map & Data Visualization App")
 st.caption("Enter city name")
 
 city = st.text_input(label_visibility = 'collapsed',label= "city name", value= "London")
-weather_and_forecast_data = fetchweather_and_forecast_data(city)
+
+weather_data = fetchweather_and_forecast_data(city, WEATHER_URL)
 
 if city:
     st.subheader(f"Current Weather in {city}")
     kpi1, kpi2, kpi3 = st.columns(3)
 
     with kpi1:
-        st.metric(label="Temperature (°C)", value= f"{weather_and_forecast_data["weather"]["main"]["temp"]:.2f}°C")
+        st.metric(label="Temperature (°C)", value= f"{weather_data["main"]["temp"]:.2f}°C")
     
     with kpi2:
-        st.metric(label="Humidity (%)", value= f"{weather_and_forecast_data["weather"]["main"]["humidity"]:.0f}%")
+        st.metric(label="Humidity (%)", value= f"{weather_data["main"]["humidity"]:.0f}%")
 
     with kpi3:
-        st.metric(label="Wind Speed (m/s)", value= f"{weather_and_forecast_data["weather"]["wind"]["speed"]:.2f}m/s")
+        st.metric(label="Wind Speed (m/s)", value= f"{weather_data["wind"]["speed"]:.2f}m/s")
     
     st.subheader("Weather Map")
-    map_data = pd.DataFrame({'latitude': [weather_and_forecast_data["weather"]["coord"]["lat"]], "longitude": [weather_and_forecast_data["weather"]["coord"]["lon"]]})
+    map_data = pd.DataFrame({'latitude': [weather_data["coord"]["lat"]], "longitude": [weather_data["coord"]["lon"]]})
     st.map(map_data)
 else:
      st.error("No data available.")
 
 #Forecast
+forecast_data = fetchweather_and_forecast_data(city, FORECAST_URL)
+
 st.subheader("Temperature Trends (Next 5 Days)")
 
-if "list" in weather_and_forecast_data["forecast"]:
-    df = pd.DataFrame(weather_and_forecast_data["forecast"]["list"])
+if "list" in forecast_data:
+    df = pd.DataFrame(forecast_data["list"])
     times = df["dt_txt"]
     temps = []
     for i in range(0, 40):
