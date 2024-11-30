7bb8f99b082432884955a611cf4922339a576fc1
Methmat <nemethmate1989@gmail.com>
2024-11-30 12:54:18 +0100

    nemethmate_homework_update_2

--- homeworks/nemethmate/8_streamlit/forecast.py
+++ homeworks/nemethmate/8_streamlit/forecast.py
@@ -1,7 +1,18 @@
 import requests
+import streamlit as st
 
-def get_forecast(city, api_key):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
+@st.cache_data
+def fetch_data(city, api_key, endpoint):
+    """
+    Általános API-hívás az OpenWeatherMap szolgáltatáshoz.
+    Paraméterek:
+        city (str): Város neve
+        api_key (str): API kulcs
+        endpoint (str): Az API végpontja ('weather' vagy 'forecast')
+    Visszatérési érték:
+        dict: Az API válasz JSON formátumban, vagy None, ha hiba történt.
+    """
+    url = f"http://api.openweathermap.org/data/2.5/{endpoint}?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     if response.status_code == 200:
         return response.json()
