34b28458963c4b1742ecb40e2e8cda04c5a3627b
vattti <attila.veizer@outlook.hu>
2024-12-01 18:54:19 +0100

    9. házi feladat (3 .py állomány)
    Szia István! Itt küldüm a FastAPI házi feladatot. Izgalmas téma, bár egyre meredekebb a pálya :) Viszont az előadás újbóli visszanézése és maga az órai kódbázis sokat segített, alapvetően én is erre építettem.
    
    Köszönöm és üdv.: Attila

---
+++ homeworks/veizerattila/9_fastapi/products_api.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from uuid import UUID
+from contextlib import asynccontextmanager
+from models import Product, ProductRequest, ProductResponse
+from database import Base, engine, get_db
+from typing import List
+
+# cd 9_fastapi\products_api.py
+# uvicorn products_api:app --reload
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield  # Allows the app to actually run after finish startup tasks
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/products/", response_model=List[ProductResponse])
+async def get_products(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Product)) #SELECT * FROM products
+    products = result.scalars().all()
+    return products
+
+@app.post("/products/", response_model=ProductResponse)
+async def add_product(product: ProductRequest, db: AsyncSession = Depends(get_db)):
+    new_product = Product(**product.dict())
+    db.add(new_product)
+    await db.commit()
+    await db.refresh(new_product)
+    return new_product
+
+@app.get("/products/{product_id}", response_model=ProductResponse)
+async def get_product(product_id: UUID, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Product).where(Product.id == str(product_id)))
+    movie = result.scalar_one_or_none() 
+    if not movie: #guard clause
+        raise HTTPException(status_code=404, detail=f"Product id {product_id} not found")
+    return movie
+
+@app.put("/products/{product_id}", response_model=ProductResponse)
+async def update_product(product_id: UUID, product_update: ProductRequest, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Product).where(Product.id == str(product_id)))
+    product = result.scalar_one_or_none()
+    if not product: #guard clause
+        raise HTTPException(status_code=404, detail=f"Product id {product_id} not found")
+
+    for key, value in product_update.model_dump(exclude_unset=True).items():
+        setattr(product, key, value) # product update
+
+    db.add(product)
+    await db.commit()
+    await db.refresh(product)
+    return product
+
+@app.delete("/products/{product_id}", response_model=ProductResponse)
+async def delete_product(product_id: UUID, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Product).where(Product.id == str(product_id)))
+    product = result.scalar_one_or_none()
+    if not product: #guard clause
+        raise HTTPException(status_code=404, detail=f"Product id {product_id} not found")
+    
+    await db.delete(product)
+    await db.commit()
+    return product
