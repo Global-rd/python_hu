d1e894055771dc880b35489aa15929dd6c49020b
Norbert Nagy <norbert.1.nagy@nokia.com>
2024-12-01 10:17:09 +0100

    Homework - 9

---
+++ homeworks/nagynorbert/9_fastapi/webshop_database.py
@@ -0,0 +1,15 @@
+#Homework - Nagy Norbert
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite+aiosqlite:///./webshopitems.db"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
