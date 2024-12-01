b15054258d9d9e34aca817e2c8a15aedbee5d7dc
zsoltcsengeri <zsoltcsengeri@yahoo.com>
2024-12-01 16:57:32 +0000

    CRUD fully implemented

--- homeworks/zsoltcsengeri/9_fastapi/routers_products_crud.py
+++ homeworks/zsoltcsengeri/9_fastapi/routers_products_crud.py
@@ -20,9 +20,10 @@ app = FastAPI(lifespan=lifespan)
 
 @app.get("/products/", response_model=List[ProductResponse])
 async def get_products(db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(Product)) #SELECT * FROM products
+    result = await db.execute(select(Product))  # SELECT * FROM products
     products = result.scalars().all()
     return products
+
 
 @app.post("/products/", response_model=ProductResponse)
 async def add_product(product: ProductRequest, db: AsyncSession = Depends(get_db)):
@@ -32,10 +33,44 @@ async def add_product(product: ProductRequest, db: AsyncSession = Depends(get_db
     await db.refresh(new_product)
     return new_product
 
+
 @app.get("/products/{product_id}", response_model=ProductResponse)
 async def get_product(product_id: UUID, db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Product).where(Product.id == str(product_id)))
-    product = result.scalar_one_or_none() 
-    if not product: #guard clause
+    product = result.scalar_one_or_none()
+    if not product:  # guard clause
+        raise HTTPException(
+            status_code=404, detail=f"Product id {product_id} not found"
+        )
+    return product
+
+
+@app.put("/products/{product_id}", response_model=ProductResponse)
+async def update_product(
+    product_id: UUID, product_update: ProductRequest, db: AsyncSession = Depends(get_db)
+):
+    result = await db.execute(select(Product).where(Product.id == str(product_id)))
+    product = result.scalar_one_or_none()
+    if not product:  # guard clause
+        raise HTTPException(
+            status_code=404, detail=f"product id {product_id} not found"
+        )
+
+    for key, value in product_update.model_dump(exclude_unset=True).items():
+        setattr(product, key, value)
+
+    db.add(product)
+    await db.commit()
+    await db.refresh(product)
+    return product
+
+@app.delete("/products/{product_id}", response_model=ProductResponse)
+async def delete_product(product_id: UUID, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Product).where(Product.id == str(product_id)))
+    product = result.scalar_one_or_none()
+    if not print: #guard clause
         raise HTTPException(status_code=404, detail=f"Product id {product_id} not found")
+    
+    await db.delete(product)
+    await db.commit()
     return product
