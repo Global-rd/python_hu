c311c083fec4697199bfe46b8ca26f5b708b70ab
Módos András <modos.andras@outlook.com>
2024-12-03 16:13:35 +0100

    2. házi végleges verzió

---
+++ homeworks/modosandras/homework-02/fictive_character.py
@@ -0,0 +1,11 @@
+name = input("Név: ")
+age = input("Életkor: ")
+experience = input("Python tapasztalat: ")
+
+name = name.strip().capitalize()
+
+age = int(age)
+
+age_in_days = age * 365
+
+print(f"My character is {age_in_days} days old. His/her name is {name} and he/she has {experience} years experience with Python.")
