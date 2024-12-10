f2a2c2cad54aa2c2d8988d1d61d69ace1de34a5d
DebZol <66560526+DebZol@users.noreply.github.com>
2024-12-10 16:33:01 +0100

    Megpróbálom

---
+++ homeworks/debreczenizoltan/4_exceptions_logging/to_do.py
@@ -0,0 +1,37 @@
+﻿# to_do.py
+import time
+    
+def add():
+    print("1-es válasz")
+    time.sleep(2)
+    
+
+def view():
+    print("2-es válasz")
+    time.sleep(2)
+
+def remove():
+    print("3-es válasz")
+    time.sleep(2)
+
+def display_menu():
+    while True :
+        print("  1. Add Task\n  2. View Tasks\n  3. Remove Task\n  4. Exit")    
+        
+        valasz = input("Válassz : ")
+        
+        if valasz == '1' :
+            add()
+        elif valasz == '2' :
+            view()
+        elif valasz == '3' :
+            remove()
+        elif valasz == '4' :
+            print("Kilépés!")
+            break    
+        else : 
+            print("Érvénytelen választás !")
+            time.sleep(2)    
+
+display_menu()
+    
